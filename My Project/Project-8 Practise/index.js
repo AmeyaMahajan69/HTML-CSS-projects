d388ca17721071a57322ea44fa00e9d93b246285
AmeyaMahajan69 <59506653+AmeyaMahajan69@users.noreply.github.com>
2023-08-29 21:40:54 +0530

    Add files via upload

---
+++ My Project/Project-8 Practise/index.js
@@ -0,0 +1,7 @@
+var Btn = document.getElementById("Btn");
+      var light = document.getElementById("Light");
+
+      function toggleBtn() {
+        Btn.classList.toggle("active");
+        light.classList.toggle("on");
+      }
